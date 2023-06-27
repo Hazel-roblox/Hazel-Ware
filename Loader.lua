cf194f1f41c2cf568637ecfda4eb6be29700f2b7
FemboyLover <123281638+FemboyLover1@users.noreply.github.com>
2023-06-27 12:23:49 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
-if game.GameId == 6872274481 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/Games/betarelease.lua", true))()
-elseif game.GameId == 6872265039 then
+if game.PlaceId == 6872265039 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/Games/bedwarslobby.lua", true))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hazel-roblox/Hazel-Ware/main/Games/betarelease.lua", true))()
 end
